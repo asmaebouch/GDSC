bf3565bcba0e40de412a120974445b36e43be231
asmaebouch <asmae.bouch@gmaail.com>
2022-12-03 16:14:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  <div className="user">
+   <h3 className ="name" />Asmae Bouchekara
+   <p className="post"/> full stack developer
+  </div>
+  <nav class="navbar">
+    <ul>
+
+      <li><a href="#home" /> home </li>
+          <li><a href="#about" /> about </li>
+                <li><a href="#education" /> education </li>
+      <li><a href="#portfolio" /> portfolio </li>
+
+
+    </ul>
+  </nav>
+</div>
+    )
+}
+
+export default App;
